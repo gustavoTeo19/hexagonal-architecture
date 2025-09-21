5f2973212f3b137fafd07a59fc1fc6288e0e2c06
Gustavo Mamede <gustavo.mamede1908@gmail.com>
2025-09-21 12:11:42 -0300

    adicionando caso de uso para buscar um customer por id

---
+++ src/main/java/com/mamede/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -0,0 +1,17 @@
+package com.mamede.hexagonal.application.core.usecase;
+
+import com.mamede.hexagonal.application.core.domain.Customer;
+import com.mamede.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id){
+        return findCustomerByIdOutputPort.find(id).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+}
